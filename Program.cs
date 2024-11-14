e153aa7e76351da8eeebbc140d945d57a39eae9f
Dark202407 <danielgutierrez159@gmail.com>
2024-11-13 23:05:06 -0500

    Segundo Commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    static int[] valores = new int[6];
+    private static int totalvalores = valores.Length;
+    private static void Main(string[] args)
+    {
+        string[] valores = { "uno", "dos", "tres", "cuatro", "cinco", "seis" };
+
+        System.Out.println(Arrays.Tostring(valores));
+        removeindex(valores, 0);
+        System.Out.println(Arrays.Tostring(valores));
+    }
+
+    private static void removeindex(string[] array, int index)
+    {
+        int i = index;
+        for (; i < array.Length - 1; i++)
+        {
+            array[i] = array[i + 1];
+        }
+        array[i] = null;
+    }
+}
